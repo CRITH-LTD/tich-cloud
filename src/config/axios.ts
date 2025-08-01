c21b876cb51046313d2aff40a56e8f2846bc7ab7
Ojage <hismajestylordkratos@gmail.com>
2025-08-01 16:16:27 +0100

    `Switched between local and remote base URLs in axios configuration`

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
-  // baseURL: 'https://tich-sl9r.onrender.com',
+  // baseURL: 'http://localhost:8000',
+  baseURL: 'https://tich-sl9r.onrender.com',
   // baseURL: 'http://192.168.0.114:8000',
 });
 
